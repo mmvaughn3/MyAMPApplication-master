1c3447e44e657a8efecca763be306d31395d4f24
cidseuser <cidseuser>
2016-02-06 10:14:26 -0700

    Initial commit

---
+++ MyAMPApplication-master/app/src/main/java/com/example/cosorio/myampapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.cosorio.myampapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private SeekBar textSize = null;
+    private SeekBar second = null;
+    private TextView textToGrow = null;
+    private RatingBar rate = null;
+    private int FiveStars = 5;
+    static final String console_text ="AMPconsole_string";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Log.i(console_text, "Entered onCreate Function");
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        textSize = (SeekBar) findViewById(R.id.seekBar1);
+        second = (SeekBar) findViewById(R.id.seekBar2);
+        textToGrow = (TextView) findViewById(R.id.editText);
+        rate = (RatingBar) findViewById(R.id.ratingBar);
+        rate.setMax(FiveStars);
+
+        textSize.setOnSeekBarChangeListener(new SeekBar. int progress. boolean fromUser);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        Log.i(console_text, "Entered onCreateOptionsMenu Function");
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
